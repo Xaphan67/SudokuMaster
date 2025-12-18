ea868b7b026ca9a0c1d7f18122f2d719a80a3beb
Gauthier GLAD.CHAMBET <gauthier.glad.chambet@labom2iformation.fr>
2025-12-18 14:40:35 +0100

    Ajout des vues

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,8 +1,8 @@
     <footer>
         <a><img src="https://placehold.co/124x40/000000/FFFFFF/png" alt=""></a>
         <a><img src="https://placehold.co/124x40/000000/FFFFFF/png" alt=""></a>
-        <a>Mentions légales</a>
-        <a>Gestion des Cookies</a>
+        <a href="index.php?action=legals">Mentions légales</a>
+        <a href="index.php?action=gcu">Conditions Générales d'Utilisation</a>
     </footer>
     <script src="#########insérer_chemin_fichier_script.js##########"></script>
   </body>
--- view/partials/header.php
+++ view/partials/header.php
@@ -2,13 +2,13 @@
     <header>
         <img src="https://placehold.co/132x54/000000/FFFFFF/png" alt="">
         <nav>
-            <a href="">Jeu Solo</a>
+            <a href="index.php?controller=partie&action=soloBoard">Jeu Solo</a>
             <a href="">Multijoueur</a>
-            <a href="">Classements</a>
-            <a href="">Règles</a>
+            <a href="index.php?action=leaderboard">Classements</a>
+            <a href="index.php?action=rules">Règles</a>
         </nav>
         <div>
-            <a href="">Connexion</a>
+            <a href="index.php?controller=utilisateur&action=login">Connexion</a>
             <img src="https://placehold.co/54x54/000000/FFFFFF/png" alt="">
         </div>
     </header>
