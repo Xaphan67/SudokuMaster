7e729cc08d675893675cfb94e37aafc1d42668be
Xaphan67 <cedric.falda@gmail.com>
2025-12-18 11:28:53 +0100

    Ajout fichier base de données

---
+++ sudoku_master.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 18 déc. 2025 à 10:07
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `sudoku_master`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classer`
+--
+
+DROP TABLE IF EXISTS `classer`;
+CREATE TABLE IF NOT EXISTS `classer` (
+  `id_classer` int NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int NOT NULL,
+  `id_mode` int NOT NULL,
+  `score_global` int NOT NULL,
+  `grilles_jouees` int NOT NULL,
+  `grilles_resolues` int NOT NULL,
+  `temps_moyen` time NOT NULL,
+  `meilleur_temps` time NOT NULL,
+  `serie_victoires` int NOT NULL,
+  PRIMARY KEY (`id_classer`) USING BTREE,
+  KEY `id_utilisateur` (`id_utilisateur`),
+  KEY `id_mode` (`id_mode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `difficulte`
+--
+
+DROP TABLE IF EXISTS `difficulte`;
+CREATE TABLE IF NOT EXISTS `difficulte` (
+  `id_difficulte` int NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_difficulte`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mode`
+--
+
+DROP TABLE IF EXISTS `mode`;
+CREATE TABLE IF NOT EXISTS `mode` (
+  `id_mode` int NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_mode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `participer`
+--
+
+DROP TABLE IF EXISTS `participer`;
+CREATE TABLE IF NOT EXISTS `participer` (
+  `id_participer` int NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int NOT NULL,
+  `id_partie` int NOT NULL,
+  `numero_salle` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_participer`),
+  KEY `id_utilisateur` (`id_utilisateur`),
+  KEY `id_partie` (`id_partie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partie`
+--
+
+DROP TABLE IF EXISTS `partie`;
+CREATE TABLE IF NOT EXISTS `partie` (
+  `id_partie` int NOT NULL AUTO_INCREMENT,
+  `id_mode` int NOT NULL,
+  `id_difficulte` int NOT NULL,
+  `duree` time DEFAULT NULL,
+  `gagnant` int DEFAULT NULL,
+  PRIMARY KEY (`id_partie`),
+  KEY `id_mode` (`id_mode`),
+  KEY `id_difficulte` (`id_difficulte`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `id_role` int NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mdp` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  KEY `id_role` (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `classer`
+--
+ALTER TABLE `classer`
+  ADD CONSTRAINT `classer_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`),
+  ADD CONSTRAINT `classer_ibfk_2` FOREIGN KEY (`id_mode`) REFERENCES `mode` (`id_mode`);
+
+--
+-- Contraintes pour la table `participer`
+--
+ALTER TABLE `participer`
+  ADD CONSTRAINT `participer_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`),
+  ADD CONSTRAINT `participer_ibfk_2` FOREIGN KEY (`id_partie`) REFERENCES `partie` (`id_partie`);
+
+--
+-- Contraintes pour la table `partie`
+--
+ALTER TABLE `partie`
+  ADD CONSTRAINT `partie_ibfk_1` FOREIGN KEY (`id_mode`) REFERENCES `mode` (`id_mode`),
+  ADD CONSTRAINT `partie_ibfk_2` FOREIGN KEY (`id_difficulte`) REFERENCES `difficulte` (`id_difficulte`);
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
