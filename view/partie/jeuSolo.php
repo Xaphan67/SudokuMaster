679c8161844ce6601b334ce38115cc383a75d612
Gauthier GLAD.CHAMBET <gauthier.glad.chambet@labom2iformation.fr>
2025-12-19 09:34:29 +0100

    Ebauche de style de l'interface de jeu

--- view/partie/jeuSolo.php
+++ view/partie/jeuSolo.php
@@ -1,5 +1,53 @@
 <main>
+    
     <p id="difficulte"></p>
-    <table id="grille">
-    </table>
+    <div id="conteneur_jeu">
+        <table id="grille">
+        </table>
+
+        <div id="interface_de_jeu"> <!-- boutons de jeu -->
+            <div> <!-- notes et temps -->
+                <div>
+                    <div><img src="" alt=""></div>
+                    <p>Notes : OFF</p>
+                </div>
+                <div>
+                    <div><img src="" alt=""></div>
+                    <p>Temps : 00:00</p>
+                </div>
+            </div>
+
+            <div id="pave_numerique"> <!-- pavé numérique -->
+                <div id="pave_1">
+                    <p>1</p>
+                </div>
+                <div id="pave_2">
+                    <p>2</p>
+                </div>
+                <div id="pave_3">
+                    <p>3</p>
+                </div>
+                <div id="pave_4">
+                    <p>4</p>
+                </div>
+                <div id="pave_5">
+                    <p>5</p>
+                </div>
+                <div id="pave_6">
+                    <p>6</p>
+                </div>
+                <div id="pave_7">
+                    <p>7</p>
+                </div>
+                <div id="pave_8">
+                    <p>8</p>
+                </div>
+                <div id="pave_9">
+                    <p>9</p>
+                </div>
+            </div>
+
+            <div id="bouton_jeu">Nouvelle Partie</div>
+        </div>
+    </div>
 </main>
