b8e20287001d954dc65c4faad276cd331d1a415c
Gauthier GLAD.CHAMBET <gauthier.glad.chambet@labom2iformation.fr>
2025-12-19 12:02:04 +0100

    Ajout de fonction de sélection de cases dans la grille de jeu et modification des styles

--- view/assets/js/jeu.js
+++ view/assets/js/jeu.js
@@ -7,8 +7,28 @@ let caseActuelle = null;
 // Stocke la case sur laquelle l'utilisateur à cliqué
 TABLE.addEventListener("click", (e) => {
     const ELEMENT_CLICK = e.target;
+    const CELLULES = Array.prototype.slice.call(TABLE.getElementsByTagName("td"));
     if (ELEMENT_CLICK.nodeName == "TD") {
         caseActuelle = ELEMENT_CLICK;
+        ELEMENT_CLICK.classList.add("selected_highlight");
+        
+         // Color toutes les cellules autres que celle selectionnée
+            CELLULES.forEach(cellule => {
+                if (cellule != e.target && e.target != 0) {
+                    cellule.classList.remove("selected_highlight");
+                    // si la cellule n'a pas le même chiffre que celle selectionné et qu'elle a la classe selected
+                    if (cellule.innerHTML != e.target.innerHTML && cellule.classList.contains("selected")) {
+                        //on enlève la classe selected
+                        cellule.classList.remove("selected");
+                    }
+                    // si la cellule a le même chiffre que celle selectionnée et que ce n'est pas vide
+                    if (cellule.innerHTML == e.target.innerHTML && e.target.innerHTML != 0) {
+                        cellule.classList.add("selected");
+                        cellule.classList.remove("selected_highlight");
+                    }
+                }
+            });
+
     }
 })
 
