1ecf834c018409a682eda7d9ee3e8210e5ea4f66
Xaphan67 <cedric.falda@gmail.com>
2026-01-12 02:54:34 +0100

    Refactorisation page d'accueil mobile first

--- view/main/accueil.php
+++ view/main/accueil.php
@@ -1,12 +1,15 @@
 <main>
     <div id="conteneur_principal" class="vertical">
         <h1>SUDOKU MASTER</h1>
+        <div id="boutons_haut">
+            <a href="index.php?controller=partie&action=soloBoard" class="bouton boutonPrincipal boutonLarge">Jouer seul</a>
+            <a href="index.php?controller=partie&action=lobby" class="bouton boutonPrincipal boutonLarge">Jouer à plusieurs</a>
+        </div>
         <section id="accueil_haut">
-            <div>
-                <img src="view/assets/img/Grille.webp" alt="">
-            </div>
+            <img src="view/assets/img/Grille.webp" alt="">
             <div>
                 <h2>Jouez à Sudoku Master gratuitement !</h2>
+                <img src="view/assets/img/Grille.webp" alt="">
                 <p>
                     Sudoku Master est un jeu de logique dont l'objectif est de compléter une grille en respectant des règles simples, faisant appel à la réflexion, à la concentration et au raisonnement.<br>
                     Pratiqué régulièrement, il contribue à stimuler les fonctions cognitives telles que la mémoire, l'attention et la capacité de résolution de problèmes.
@@ -15,7 +18,7 @@
                 <p>Sudoku Master propose plusieurs modes de jeu : un mode solo pour jouer à son rythme et améliorer ses performances, un mode compétitif pour se mesurer à d'autres joueurs et relever des défis chronométrés, ainsi qu'un mode coopératif permettant de résoudre une grille à plusieurs, en favorisant l'échange et la collaboration.</p>
             </div>
         </section>
-        <div>
+        <div id="boutons_centre">
             <a href="index.php?controller=partie&action=soloBoard" class="bouton boutonPrincipal boutonLarge">Jouer seul</a>
             <a href="index.php?controller=partie&action=lobby" class="bouton boutonPrincipal boutonLarge">Jouer à plusieurs</a>
         </div>
