62b1caca052a47d37e536e15b59320593114edbe
Xaphan67 <cedric.falda@gmail.com>
2026-01-21 12:43:54 +0100

    Ajout lien connexion dans menu utilisateur

--- view/_partials/header.php
+++ view/_partials/header.php
@@ -93,18 +93,22 @@
                             <label for="mode_dyslexique" class="switch_label"></label>
                         </div>
                     </div>
-                    <?php
-                        if(isset($_SESSION["utilisateur"])) {
-                    ?>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-                                <path d="M110 216a6 6 0 0 1-6 6H48a14 14 0 0 1-14-14V48a14 14 0 0 1 14-14h56a6 6 0 0 1 0 12H48a2 2 0 0 0-2 2v160a2 2 0 0 0 2 2h56a6 6 0 0 1 6 6Zm110.24-92.24l-40-40a6 6 0 0 0-8.48 8.48L201.51 122H104a6 6 0 0 0 0 12h97.51l-29.75 29.76a6 6 0 1 0 8.48 8.48l40-40a6 6 0 0 0 0-8.48Z"/>
-                            </svg>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                            <path d="M110 216a6 6 0 0 1-6 6H48a14 14 0 0 1-14-14V48a14 14 0 0 1 14-14h56a6 6 0 0 1 0 12H48a2 2 0 0 0-2 2v160a2 2 0 0 0 2 2h56a6 6 0 0 1 6 6Zm110.24-92.24l-40-40a6 6 0 0 0-8.48 8.48L201.51 122H104a6 6 0 0 0 0 12h97.51l-29.75 29.76a6 6 0 1 0 8.48 8.48l40-40a6 6 0 0 0 0-8.48Z"/>
+                        </svg>
+                            <?php
+                                if(!isset($_SESSION["utilisateur"])) {
+                            ?>
+                            <a href="connexion">Se connecter</a>
+                            <?php
+                                } else {
+                            ?>
                             <a href="deconnexion">Se déconnecter</a>
-                        </div>
-                    <?php
-                        }
-                    ?>
+                            <?php
+                                } 
+                            ?>                   
+                    </div>
                 </div>
             </div>
         </div>
