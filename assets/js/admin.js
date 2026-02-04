a75497f5446a3969ee8315e5b01d1f6a4d354376
Xaphan67 <cedric.falda@gmail.com>
2026-02-04 17:09:24 +0100

    Ajout popup info utilisateur sur page admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,13 +3,17 @@ const CONTENEUR_PRINCIPAL = document.getElementById("conteneur_principal");
 const POPUP_SUPPRIMER_UTILISATEUR = document.getElementById("supprimer_utilisateur");
 const POPUP_SUPPRIMER_UTILISATEUR_OUI = document.getElementById("bouton_supprimer");
 const POPUP_SUPPRIMER_UTILISATEUR_NON = document.getElementById("bouton_annuler");
+const POPUP_INFOS_UTILISATEUR = document.getElementById("infos_utilisateur");
+const POPUP_INFOS_UTILISATEUR_FERMER = document.getElementById("bouton_fermer");
 const ACTIONS = Array.from(document.getElementsByClassName("actions"));
 const BOUTONS_ATIONS = Array.from(document.getElementsByClassName("boutonActions"));
+const BOUTONS_INFOS = Array.from(document.getElementsByClassName("bouton-info"));
 const BOUTONS_SUPPRIMER = Array.from(document.getElementsByClassName("bouton-supprimer"));
 const BOUTONS_FERMER = Array.from(document.getElementsByClassName("bouton-fermer"));
 
 // Variables
 let IdUtilisateur = null;
+let infosUtilisateur = null;
 
 // Pour chaque bouton d'action...
 BOUTONS_ATIONS.forEach(bouton => {
@@ -32,6 +36,20 @@ BOUTONS_ATIONS.forEach(bouton => {
     });
 });
 
+// Pour chaque bouton info
+BOUTONS_INFOS.forEach(bouton => {
+
+    // Lorsqu'on clique dessus...
+    bouton.addEventListener("click", (e) => {
+
+        // stocke l'ID de l'utilisateur correspondant
+        IdUtilisateur = bouton.getAttribute("name").split('-')[1];
+
+        // Récupère les informations de l'utilisateur
+        getUserInfos(IdUtilisateur);
+    });
+});
+
 // Pour chaque bouton de suppression...
 BOUTONS_SUPPRIMER.forEach(bouton => {
 
@@ -49,7 +67,7 @@ BOUTONS_SUPPRIMER.forEach(bouton => {
             CONTENEUR_PRINCIPAL.inert = true;
             POPUP_SUPPRIMER_UTILISATEUR.style.display = "flex";
 
-            // Lors du clic que le bouton Oui
+            // Lors du clic sur le bouton Oui
             POPUP_SUPPRIMER_UTILISATEUR_OUI.addEventListener("click", (e) => {
                 
                 fetch("index.php?controller=api-utilisateur&action=delete", {
@@ -64,7 +82,7 @@ BOUTONS_SUPPRIMER.forEach(bouton => {
                 window.location.replace("administration");
             });
 
-            // Lors du clic que le bouton Non
+            // Lors du clic sur le bouton Non
             POPUP_SUPPRIMER_UTILISATEUR_NON.addEventListener("click", (e) => {
                 
                 // Masque le popup de confirmation
@@ -90,3 +108,163 @@ BOUTONS_FERMER.forEach(bouton => {
         ACTIONS.style.display = "none";
     });
 });
+
+async function getUserInfos() {
+
+    // Récupère les infos de l'utilisateur
+    const INFOS = await fetch("index.php?controller=api-utilisateur&action=getInfos", {
+        method: "POST",
+        headers: {
+                'Content-Type': 'application/json', // Indique qu'on envoie du JSON
+                'Accept': 'application/json' // Indique qu'on attend du JSON en réponse
+            },
+            body: JSON.stringify({id: IdUtilisateur}) // Objet JS converti en chaîne JSON
+    });
+    let resInfos = await INFOS.json();
+
+    // Affiche les informations dans le popup
+    const TITRE = POPUP_INFOS_UTILISATEUR.getElementsByTagName("H3")[0];
+    TITRE.innerText = resInfos[1].pseudo_utilisateur;
+
+    const ONGLETS = POPUP_INFOS_UTILISATEUR.getElementsByClassName("onglets")[0];
+
+    for (i = 1; i <= 3; i++) {
+
+        const CONTENEUR = document.createElement("DIV");
+        CONTENEUR.setAttribute("name","conteneur");
+        CONTENEUR.id = "mode_" + (resInfos[i].id_mode_de_jeu == 1 ? "solo" : (resInfos[i].id_mode_de_jeu == 2 ? "cooperatif" : "competitif"));
+        if (i >= 2) {
+            CONTENEUR.style.display = "none";
+        }
+
+        let grillesJouees = document.createElement("DIV");
+        let grillesJoueesTexte = document.createElement("P");
+        let grillesJoueesValeur = document.createElement("P");
+        grillesJoueesTexte.innerText = "Grilles jouées :";
+        grillesJoueesValeur.innerText = resInfos[i].grilles_jouees;
+        grillesJouees.appendChild(grillesJoueesTexte);
+        grillesJouees.appendChild(grillesJoueesValeur);
+        CONTENEUR.appendChild(grillesJouees);
+
+        let grillesResolues = document.createElement("DIV");
+        let grillesResoluesTexte = document.createElement("P");
+        let grillesResoluesValeur = document.createElement("P");
+        grillesResoluesTexte.innerText = "Grilles résolues :";
+        grillesResoluesValeur.innerText = resInfos[i].grilles_resolues;
+        grillesResolues.appendChild(grillesResoluesTexte);
+        grillesResolues.appendChild(grillesResoluesValeur);
+        grillesJouees.insertAdjacentElement("afterend", grillesResolues);
+
+        let tempsMoyen = document.createElement("DIV");
+        let tempsMoyenTexte = document.createElement("P");
+        let tempsMoyenValeur = document.createElement("P");
+        tempsMoyenTexte.innerText = "Temps moyen :";
+        tempsMoyenValeur.innerText = resInfos[i].temps_moyen;
+        tempsMoyen.appendChild(tempsMoyenTexte);
+        tempsMoyen.appendChild(tempsMoyenValeur);
+        grillesResolues.insertAdjacentElement("afterend", tempsMoyen);
+
+        let meilleurTemps = document.createElement("DIV");
+        let meilleurTempsTexte = document.createElement("P");
+        let meilleurTempsValeur = document.createElement("P");
+        meilleurTempsTexte.innerText = "Meilleur temps :";
+        meilleurTempsValeur.innerText = resInfos[i].meilleur_temps;
+        meilleurTemps.appendChild(meilleurTempsTexte);
+        meilleurTemps.appendChild(meilleurTempsValeur);
+        tempsMoyen.insertAdjacentElement("afterend", meilleurTemps);
+
+        let serieVictoires = document.createElement("DIV");
+        let serieVictoiresTexte = document.createElement("P");
+        let serieVictoiresValeur = document.createElement("P");
+        serieVictoiresTexte.innerText = "Série de victoires :";
+        serieVictoiresValeur.innerText = resInfos[i].serie_victoires;
+        serieVictoires.appendChild(serieVictoiresTexte);
+        serieVictoires.appendChild(serieVictoiresValeur);
+        meilleurTemps.insertAdjacentElement("afterend", serieVictoires);
+
+        ONGLETS.insertAdjacentElement("afterend", CONTENEUR);
+    }
+
+    const ONGLET_SOLO = document.getElementById("mode_solo");
+    const ONGLET_COOPERATIF = document.getElementById("mode_cooperatif");
+    const ONGLET_COMPETITIF = document.getElementById("mode_competitif");
+
+    // Affiche l'onglet "Solo"
+    ONGLETS.getElementsByTagName("P")[0].addEventListener("click", (e) => {
+        if (!e.target.classList.contains("onglet_actif")) {
+            // Retire la classe onglet_actif de tout les onglets, puis l'ajoute à l'onglet "Solo"
+            Array.from(ONGLETS.getElementsByTagName("P")).forEach(element => {
+                element.classList.remove("onglet_actif");
+            });
+            ONGLETS.getElementsByTagName("P")[0].classList.add("onglet_actif");
+
+            // Affiche l'onglet "Solo" et masque les autres onglets
+            ONGLET_SOLO.style.display = "block";
+            ONGLET_COOPERATIF.style.display = "none";
+            ONGLET_COMPETITIF.style.display = "none";
+        }
+    });
+
+    // Affiche l'onglet "Cooperatif"
+    ONGLETS.getElementsByTagName("P")[1].addEventListener("click", (e) => {
+        if (!e.target.classList.contains("onglet_actif")) {
+            // Retire la classe onglet_actif de tout les onglets, puis l'ajoute à l'onglet "Cooperatif"
+            Array.from(ONGLETS.getElementsByTagName("P")).forEach(element => {
+                element.classList.remove("onglet_actif");
+            });
+        ONGLETS.getElementsByTagName("P")[1].classList.add("onglet_actif");
+
+            // Affiche l'onglet "Cooperatif" et masque les autres onglets
+            ONGLET_SOLO.style.display = "none";
+            ONGLET_COOPERATIF.style.display = "block";
+            ONGLET_COMPETITIF.style.display = "none";
+        }
+    });
+
+    // Affiche l'onglet "Competitif"
+    ONGLETS.getElementsByTagName("P")[2].addEventListener("click", (e) => {
+        if (!e.target.classList.contains("onglet_actif")) {
+            // Retire la classe onglet_actif de tout les onglets, puis l'ajoute à l'onglet "Competitif"
+            Array.from(ONGLETS.getElementsByTagName("P")).forEach(element => {
+                element.classList.remove("onglet_actif");
+            });
+            ONGLETS.getElementsByTagName("P")[2].classList.add("onglet_actif");
+
+            // Affiche l'onglet "Competitif" et masque les autres onglets
+            ONGLET_SOLO.style.display = "none";
+            ONGLET_COOPERATIF.style.display = "none";
+            ONGLET_COMPETITIF.style.display = "block";
+        }
+    });
+
+    // Affiche le popup d'informations
+    CONTENEUR_PRINCIPAL.style.filter = "opacity(0.40)";
+    CONTENEUR_PRINCIPAL.inert = true;
+    POPUP_INFOS_UTILISATEUR.style.display = "flex";
+
+    // Lors du clic sur le bouton Fermer
+    POPUP_INFOS_UTILISATEUR_FERMER.addEventListener("click", (e) => {
+        
+        // Masque le popup de confirmation
+        CONTENEUR_PRINCIPAL.style.filter = "none";
+        CONTENEUR_PRINCIPAL.inert = false;
+        POPUP_INFOS_UTILISATEUR.style.display = "none";
+
+        // Supprime les informations du popup
+        const CONTENEURS = Array.from(document.getElementsByName("conteneur"));
+        CONTENEURS.forEach(conteneur => {
+            conteneur.remove();
+        });
+
+        // Retire la classe onglet_actif de tout les onglets, puis l'ajoute à l'onglet "Solo"
+        Array.from(ONGLETS.getElementsByTagName("P")).forEach(element => {
+            element.classList.remove("onglet_actif");
+        });
+        ONGLETS.getElementsByTagName("P")[0].classList.add("onglet_actif");
+
+        // Affiche l'onglet "Solo" et masque les autres onglets
+        ONGLET_SOLO.style.display = "block";
+        ONGLET_COOPERATIF.style.display = "none";
+        ONGLET_COMPETITIF.style.display = "none";
+    });
+}
--- src/Api/UtilisateurApi.php
+++ src/Api/UtilisateurApi.php
@@ -2,6 +2,8 @@
 
 namespace Xaphan67\SudokuMaster\Api;
 
+use Xaphan67\SudokuMaster\Entities\Classer;
+use Xaphan67\SudokuMaster\Models\ClasserModel;
 use Xaphan67\SudokuMaster\Entities\Utilisateur;
 use Xaphan67\SudokuMaster\Models\UtilisateurModel;
 
@@ -22,5 +24,37 @@ class UtilisateurApi {
 
         // Supprime l'utilisateur en base de données
         $utilisateurSupprime = $utilisateurModel->delete($utilisateur);
+    }
+
+    function getInfos() {
+
+        // Récupération des données envoyées par JS
+        $json_data = file_get_contents('php://input'); // Lit le corps brut de la requête
+        $dataJS = json_decode($json_data, true); // Décode le JSON en tableau associatif
+
+        // Crée une instance du modèle Classer et appelle la méthode
+        // pour récupérer les statistiques en base de donnée
+        $classerModel = new ClasserModel;
+
+        // Instancie un tableau qui contiendra les statistiques
+        $statistiques = [];
+
+        // Pour chaque mode de jeu, récupère les statistiques
+        for($mode = 1; $mode <= 3; $mode++) {
+            $donneesClasser = $classerModel->findByUserAndMode($dataJS["id"], $mode, true);
+
+            // S'il y à des statistiques pour le mode
+            if ($donneesClasser) {
+
+                // Retire l'affichage des heures (qui sera toujours 00:) et tronque les millisecondes
+                $donneesClasser["temps_moyen"] = substr($donneesClasser["temps_moyen"], 3, 5);
+                $donneesClasser["meilleur_temps"] = substr($donneesClasser["meilleur_temps"], 3, 5);
+
+                // Ajoute l'objet Classer au tableau de statistiques
+                $statistiques[$mode] = $donneesClasser;
+            }
+        };
+
+        echo json_encode($statistiques);
     }
 }
